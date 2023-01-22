e08853111905bdee1c23920598eaacd42c0b8a5e
Xxxx <you@example.com>
2023-01-23 01:30:23 +0800

    change

--- train_tagger/huggingface_utils.py
+++ train_tagger/huggingface_utils.py
@@ -2,7 +2,8 @@
 import huggingface_hub
 from huggingface_hub import ModelFilter
 from huggingface_hub import HfApi
-
+from datetime import datetime, timezone
+import pytz
 #%%
 api = HfApi()
 
@@ -11,6 +12,11 @@ def model_exist(model_name):
     filter = ModelFilter(model_name =model_name)
     model = api.list_models(filter=filter)
     return len(model) > 0
+
+def model_last_uptime_time(model_name):
+    filter = ModelFilter(model_name =model_name)
+    model = api.list_models(filter=filter)[0]
+    return datetime.strptime(model.lastModified, "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
 
 def hf_login(token):
     huggingface_hub.login(token, add_to_git_credential=True)
