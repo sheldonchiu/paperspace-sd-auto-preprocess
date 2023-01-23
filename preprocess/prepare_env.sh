c452256c29435c22085de48c504473088c77febc
Xxxx <you@example.com>
2023-01-23 12:03:12 +0800

    fix protobuf version to solve issue with blip

--- preprocess/prepare_env.sh
+++ preprocess/prepare_env.sh
@@ -13,7 +13,9 @@ cd $root_path/$repo_name
 
 echo "Installing Dependencies"
 apt-get update && apt-get install -y git libgl1 libglib2.0-0 aria2 pigz
-pip install --upgrade -r requirements.txt
+pip install -U pip
+pip install protobuf==3.20.3
+pip install -r requirements.txt
 pip install realesrgan minio wandb python-logging-discord-handler
 pip install xformers==0.0.16rc425
 
