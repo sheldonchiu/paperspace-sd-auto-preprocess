17c45fcf7c30b129bf75cef88de4ffa98b412d6b
sheldonchiu <sheldon.chiu1998@gmail.com>
2023-08-13 00:16:01 +0800

    clean up

--- visualize/main.py
+++ visualize/main.py
@@ -1,25 +1,22 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
-from math import ceil
 import os
 import json
+from math import ceil
+
+import click
+from flask import Flask, render_template, jsonify, send_from_directory
 
 app = Flask(__name__)
 
-IMAGE_DIR = '/Users/sheldon/Documents/booru_data/0_filter'  # Replace with your path
-EXCLUDE_FILE = 'exclude.json'
-exclude_list = []
-accepted_formats = ['.jpg', '.png', '.jpeg', '.gif']  # Add or remove formats as needed
+IMAGE_DIR = None
+EXCLUDE_FILE = None
+EXCLUDE_LIST = []
 IMAGES_PER_PAGE = 200
-
-if os.path.isfile(EXCLUDE_FILE):
-    with open(EXCLUDE_FILE, 'r') as f:
-        exclude_list = json.load(f)
+accepted_formats = ['.jpg', '.png', '.jpeg', '.gif']  # Add or remove formats as needed
 
 @app.route('/')
 @app.route('/page/<int:page>')
 def index(page=1):
     files = [f for f in os.listdir(IMAGE_DIR) if os.path.splitext(f)[1] in accepted_formats]
-    # files = [f for f in files if f not in exclude_list]
     
     total_pages = ceil(len(files) / IMAGES_PER_PAGE)
     
@@ -27,7 +24,7 @@ def index(page=1):
     end = start + IMAGES_PER_PAGE
     files = files[start:end]
     
-    return render_template('index.html', files=files, page=page, total_pages=total_pages, exclude_list=exclude_list)
+    return render_template('index.html', files=files, page=page, total_pages=total_pages, exclude_list=EXCLUDE_LIST)
 
 @app.route('/images/<path:filename>')
 def serve_image(filename):
@@ -47,16 +44,62 @@ def get_caption(filename):
 
 @app.route('/exclude/<path:filepath>', methods=['POST'])
 def exclude(filepath):
-    if filepath not in exclude_list:
-        exclude_list.append(filepath)
+    if filepath not in EXCLUDE_LIST:
+        EXCLUDE_LIST.append(filepath)
         with open(EXCLUDE_FILE, 'w') as f:
-            json.dump(exclude_list, f)
+            json.dump(EXCLUDE_LIST, f)
     else:
-        exclude_list.remove(filepath)
+        EXCLUDE_LIST.remove(filepath)
         with open(EXCLUDE_FILE, 'w') as f:
-            json.dump(exclude_list, f)
+            json.dump(EXCLUDE_LIST, f)
     return '', 204
 
+@click.command()
+@click.option(
+    '--image_dir',
+    type=str,
+    help='Path to the image directory',
+    prompt='Enter the path to the image directory:'
+)
+@click.option(
+    '--exclude-file',
+    type=str,
+    help='Path to exclude.json',
+    prompt='Enter the path to exclude.json:'
+)
+@click.option(
+    '--images-per-page',
+    type=int,
+    default=200,
+    help='Number of images per page',
+    prompt='Enter the number of images per page:'
+)
+@click.option(
+    '--port',
+    type=int,
+    default=8000,
+    help='Port number',
+    prompt='Enter the port number:'
+)
+def main(image_dir, exclude_file, images_per_page, port):
+    """
+    Main function to run the application.
+    
+    Args:
+        image_dir (str): Path to the image directory.
+        exclude_file (str): Path to exclude.json file.
+        images_per_page (int): Number of images per page.
+    """
+    global IMAGE_DIR, EXCLUDE_FILE, IMAGES_PER_PAGE, EXCLUDE_LIST
+    IMAGE_DIR = image_dir
+    EXCLUDE_FILE = exclude_file
+    IMAGES_PER_PAGE = images_per_page
+    
+    if os.path.isfile(EXCLUDE_FILE):
+        with open(EXCLUDE_FILE, 'r') as f:
+            EXCLUDE_LIST = json.load(f)
+    
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
